37aac182684dce6dc0ff54b908c7efe37c2425b3
aniketsahu115 <sahuaniket115@gmail.com>
2024-06-02 00:03:35 +0530

    Third commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript code
+// You can write your JavaScript code here
+
+// Example: Displaying a message in the console
+console.log("Hello, world!");
+
+// HTML code
+// You can write your HTML code here
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+    <p>This is a paragraph.</p>
+    <script src="script.js"></script>
+</body>
+</html>
+
+// CSS code
+// You can write your CSS code here
+
+/* Example: Styling the heading */
+h1 {
+    color: blue;
+    font-size: 24px;
+}
